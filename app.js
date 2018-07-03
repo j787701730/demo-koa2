e69269a8695e93bca40d71fdadb968ea72e14d0d
zhengw <247276359@qq.com>
2018-07-03 17:44:18 +0800

    mysql.js

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
+
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: 'pug'
+  extension: 'html'
 }))
 
 // logger
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const router = require('koa-router')()
-
+const mysql = require('../lib/mysql.js')
 router.get('/', async (ctx, next) => {
   // await ctx.render('index', {
   //   title: 'hello '
   // })
+
+  // var _sql = 'select * from user';
+  console.log(await mysql.findAllPost());
   // ctx.body = ctx.request.query;
-  ctx.body = ctx.query;
+  // ctx.body = ctx.query;
+  ctx.body = await mysql.findAllPost();
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+  ctx.body = {
+    title: 'koa2 json'
+  }; // 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
